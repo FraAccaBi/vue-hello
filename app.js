3f64259b43348d6f4852d14c695cb779fdc604ac
FraAccaBi <francescahuda@gmail.com>
2022-04-20 14:53:21 +0200

    excercise complete commit

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ let app = new Vue ({
     el: '#app',
     data: {
         message: 'Prova, prova, Sa, sa',
-        img: 'https://picsum.photos/200/300'
+        img: 'https://picsum.photos/301/300'
+    },
+    methods: {
+        refresh(){
+            if(this.img == 'https://picsum.photos/301/300') {
+                this.img = 'https://picsum.photos/300/301'
+            } else {
+                this.img = 'https://picsum.photos/301/300'
+            }
+            
+        }
     }
 })
