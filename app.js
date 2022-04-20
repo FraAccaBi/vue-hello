38751b5fac9cb1435eb6dfa64309421f57b552f4
FraAccaBi <francescahuda@gmail.com>
2022-04-20 14:45:41 +0200

    print picture commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let app = new Vue ({
     el: '#app',
     data: {
-        message: 'Prova, prova, Sa, sa'
+        message: 'Prova, prova, Sa, sa',
+        img: 'https://picsum.photos/200/300'
     }
 })
